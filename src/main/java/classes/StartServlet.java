a9ee8a6ec99187721cfd9dc7f8d76ff1560fc8fd
yasuda <yada.taya@gmail.com>
2025-07-26 12:50:06 +0900

    プロジェクト実行で画面を開く

---
+++ src/main/java/classes/StartServlet.java
@@ -0,0 +1,39 @@
+package classes;
+
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class StartServlet
+ */
+@WebServlet("/")
+public class StartServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public StartServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		System.out.println("ok");
+		//response.getWriter().append("Served at: ").append(request.getContextPath());
+		RequestDispatcher rd = request.getRequestDispatcher("Start.jsp");
+		rd.forward(request,response);
+	}
+
+
+}
