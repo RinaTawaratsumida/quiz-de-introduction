2148d04ca8ba3ed5afec17c803e6849a837f0761
yasuda <yada.taya@gmail.com>
2025-07-26 16:45:03 +0900

    sqlへの接続機能追加

--- src/main/java/classes/QuizServlet.java
+++ src/main/java/classes/QuizServlet.java
@@ -1,6 +1,13 @@
 package classes;
 
 import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.Random;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -14,22 +21,60 @@ import javax.servlet.http.HttpServletResponse;
 @WebServlet("/QuizServlet")
 public class QuizServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-       
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
-    public QuizServlet() {
-        super();
-        // TODO Auto-generated constructor stub
-    }
+
+	/**
+	 * @see HttpServlet#HttpServlet()
+	 */
+	public QuizServlet() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
 
 	/**
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+
+		response.setContentType("text/html;charset=UTF-8");
+		PrintWriter out = response.getWriter();
+
 		// TODO Auto-generated method stub
 		System.out.println("OK");
 		//response.getWriter().append("Served at: ").append(request.getContextPath());
+		String dbUrl = "jdbc:postgresql://localhost:5432/quizdb";
+		String dbUserName = "postgres";
+		String dbPassword = "password";
+
+		Connection conn = null;
+		PreparedStatement stmt = null;
+		ResultSet rs = null;
+		try {
+			Class.forName("org.postgresql.Driver");
+			conn = DriverManager.getConnection(dbUrl, dbUserName, dbPassword);
+			Random rand = new Random();
+			int num = rand.nextInt(10) + 1;
+			System.out.println(num);
+
+		} catch (SQLException e) {
+			e.printStackTrace();
+			out.println("<p>データベースエラー: " + e.getMessage() + "</p>");
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+			out.println("<p>ドライバが見つかりません: " + e.getMessage() + "</p>");
+		} finally {
+			try {
+				if (rs != null)
+					rs.close();
+				if (stmt != null)
+					stmt.close();
+				if (conn != null)
+					conn.close();
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+		}
+
 	}
 
 }
