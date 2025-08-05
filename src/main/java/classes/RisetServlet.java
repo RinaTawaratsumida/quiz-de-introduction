b39fa709eaeb8a9ba5db534d2654b283855660c2
yasu-droid <95845809+yasu-droid@users.noreply.github.com>
2025-08-05 18:03:00 +0900

    RisetServlet.java を更新

--- src/main/java/classes/RisetServlet.java
+++ src/main/java/classes/RisetServlet.java
@@ -9,27 +9,24 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
-/**
- * Servlet implementation class RisetServlet
- */
 @WebServlet("/RisetServlet")
 public class RisetServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
-	/**
-	 * @see HttpServlet#HttpServlet()
-	 */
 	public RisetServlet() {
 		super();
-		// TODO Auto-generated constructor stub
 	}
 
 	protected void doGet(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
-		// TODO Auto-generated method stub
+		
+		// 既存のセッションを取得（falseなので無ければnull）
 		HttpSession session = request.getSession(false);
+		
+		// セッションを破棄して全てのデータをリセット
 		session.invalidate();
+		
+		// スタート画面に戻る
 		request.getRequestDispatcher("/Start.jsp").forward(request, response);
-
 	}
 }
