8ad5f38587116f2a5d671798af77d340ccfff4e5
yasu-droid <95845809+yasu-droid@users.noreply.github.com>
2025-08-05 18:03:32 +0900

    StartServlet.java を更新

--- src/main/java/classes/StartServlet.java
+++ src/main/java/classes/StartServlet.java
@@ -9,27 +9,20 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-/**
- * Servlet implementation class StartServlet
- */
-@WebServlet("/start")
+@WebServlet("/start") // URLが /start にアクセスされたらこのサーブレットが動く
 public class StartServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
     public StartServlet() {
         super();
-        // TODO Auto-generated constructor stub
     }
 
-	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
-	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		//プロジェクト起動時にStart.jspに遷移
+    // ブラウザからGETリクエストを受け取ったときの処理
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) 
+	        throws ServletException, IOException {
+		
+		// プロジェクト起動時や /start にアクセス時に Start.jsp に転送
 		RequestDispatcher rd = request.getRequestDispatcher("Start.jsp");
-		rd.forward(request,response);
+		rd.forward(request, response); 
 	}
 }
